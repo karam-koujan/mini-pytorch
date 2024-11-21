cafdf19d26422de5961b5922d82ced44aece753c
Karam Koujan <kkoujan@e3r8p13.1337.ma>
2024-11-21 22:40:44 +0100

    chore: add tensor data structure

---
+++ tensor.c
@@ -0,0 +1,26 @@
+
+enum Dtype
+{
+	FLOAT32,
+	DOUBLE,
+	INT32,
+	INT64
+};
+
+enum Device
+{
+	CPU,
+	GPU
+};
+
+struct Tensor
+{
+	void *data;
+	int *shape;
+	int *strides;
+	enum Dtype dtype;
+	enum Device device;
+	void *grad;
+	int	requires_grad;
+	int num_dims;
+}
