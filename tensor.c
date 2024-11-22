5b2236a9cf402788955b3dbe4b1a0b9b119cd98c
Karam Koujan <kkoujan@e3r3p17.1337.ma>
2024-11-23 00:37:02 +0100

    chore: add a create_stride fucntion

--- tensor.c
+++ tensor.c
@@ -14,6 +14,18 @@ int	*create_shape(va_list arg,int dim)
 	return shape;
 }
 
+int	*create_stride(int num_dims, int *shape)
+{
+	int *stride = malloc(num_dims);
+	int sum = 1;
+	for(int i = num_dims - 1; i >= 0; i--)
+	{
+		stride[i] = sum;
+		sum *= shape[i];
+	}
+	return (stride);
+}
+
 Tensor	create_empty_tensor(int dim,...)
 {
 	va_list arg;
@@ -21,6 +33,9 @@ Tensor	create_empty_tensor(int dim,...)
 
 	va_start(arg,dim);
 	tensor.shape =  create_shape(arg,dim);
+	tensor.num_dims = dim;
+	tensor.strides = create_stride(tensor.num_dims, tensor.shape);
 	va_end(arg);
 	return tensor;
 }
+
--- tensor.h
+++ tensor.h
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 enum Dtype
 {
 	FLOAT32,
