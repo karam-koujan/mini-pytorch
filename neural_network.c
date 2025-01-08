9181d90e9c99a9d1bebefc7515f1e1d45bd384ab
Karam Koujan <kkoujan@e3r1p6.1337.ma>
2025-01-08 16:00:23 +0100

    add forward pass example

--- neural_network.c
+++ neural_network.c
@@ -9,7 +9,7 @@ Tensor *Linear(Tensor *a, int in_features, int out_features, int use_bias)
 	memcpy(weights_shape,a->shape, a->num_dims);
 	weights_shape[a->num_dims - 1] = in_features;
 	weights_shape[a->num_dims - 2] = out_features;
-	Tensor *weights = tensor_rand(a->num_dims, weights_shape,0);
+	Tensor *weights = tensor_ones(a->num_dims, weights_shape,0);
 	tensor_set_require_grad(weights,1);
 	Tensor *weights_t = tensor_t(weights);
 	Tensor *result = tensor_matmul(a, weights_t);
@@ -35,11 +35,42 @@ Tensor *Relu(Tensor *a)
 	return a;
 }
 
-
+Tensor *foward(Tensor *a)
+{
+	Tensor *layer = Linear(a, a->shape[a->num_dims - 1], 5, 0);
+	layer = Relu(layer);
+	layer = Linear(layer, layer->shape[layer->num_dims - 1], 5, 0);
+	layer = Relu(layer);
+	layer = Linear(layer, layer->shape[layer->num_dims - 1], 5, 0);
+	layer = Relu(layer);
+	return Linear(layer, layer->shape[layer->num_dims - 1], 1, 0);
+}
+Tensor *cost(Tensor *pred, Tensor *label)
+{
+	Tensor *res = tensor_sub(pred,label);
+	return (tensor_pairwise_mul(res,res));
+}
 int main()
 {
-	int a_shape[3] = {64,128,20};
-	Tensor *a = tensor_full(3,a_shape,3.0,0);
-	Tensor *linear = Linear(a,20,30,0);
-	
+	float data[4][2] = {
+	    {0.1, 0.2},
+	    {0.3, 0.4},
+	    {0.5, 0.6},
+	    {0.7, 0.8}
+	};
+	float labels[4] = {0.3, 0.7, 1.1, 1.5};
+	int data_shape[] = {4,2};
+	Tensor *d = tensor_tensor(data,data_shape,2);
+	int label_shape[] = {4,1};
+	Tensor *l = tensor_tensor(labels,label_shape,2);
+	tensor_print(d);
+	tensor_print(l);
+	Tensor *prediction;
+	for(int epoch = 0; epoch < 50; epoch++)
+	{
+		prediction = foward(d);
+	}
+	tensor_print(prediction);
+	Tensor *cost_fn = cost(prediction, l);
+	tensor_print(cost_fn);
 }
