f71c5f63641da1d3b320ec8d5441eb979139acbf
Karam Koujan <kkoujan@e1r9p3.1337.ma>
2025-10-15 20:07:08 +0100

    mat_operation: start

--- tensor_operation/math_operations.c
+++ tensor_operation/math_operations.c
@@ -5,5 +5,19 @@
 
 Tensor *tensor_add(Tensor*a, Tensor *b)
 {
+    if (tensor_broadcast(a,b))
+        return (NULL);
+   // if they have not the same datatype promote datatype
+   
+   
+   // create a tensor_full with the newshape
 
+
+   // preform the operation, I think I should create a  shared helper function that do pairwise  operations
 }
+
+
+Tensor *tensor_sub(Tensor*a, Tensor *b);
+Tensor *tensor_matmul(Tensor*a, Tensor *b);
+Tensor *tensor_mul(Tensor*a, Tensor *b);
+Tensor *tensor_mm(Tensor*a, Tensor *b);
