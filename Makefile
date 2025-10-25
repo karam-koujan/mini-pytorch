3c928177596b331b4d61a9e51f909b2706c842ad
Karam Koujan <kkoujan@e1r9p4.1337.ma>
2025-10-25 15:54:18 +0100

    working on neural network

--- Makefile
+++ Makefile
@@ -4,7 +4,7 @@ CFLAGS = -g -Wall -Wextra -Werror
 OBJ = tensor.o ./lib/print.o ./tensor_creation/helpers.o ./tensor_creation/data_creation.o ./tensor_creation/tensor_creation.o \
 ./tensor_operation/broadcasting.o ./tensor_creation/tensor_free.o ./tensor_operation/view.o ./tensor_operation/reshape.o \
 ./tensor_operation/transpose.o ./tensor_operation/permute.o ./tensor_operation/math_operations.o ./autograd/autograd.o \
-./tensor_operation/tensor_matmul.o
+./tensor_operation/tensor_matmul.o ./neural_network/linear.o ./neural_network/helper.o
 
 HEADERS = ./headers/*
 
--- neural_network/linear.c
+++ neural_network/linear.c
@@ -4,15 +4,36 @@
 
 
 
-Tensor *Parameter(Module *m, Tensor *a)
+Tensor *Parameter(Module *m, Tensor *a, int requires_grad)
 {
-    size_t size;
-    if (!m->parameters)
+    size_t nmemb = parameters_len(m->parameters);
+    if (!a->requires_grad && requires_grad)
     {
-        size = 2 * sizeof(Tensor *);
+        tensor_set_require_grad(a, 1);
+    }
+    Tensor **new_parameters;
+    if (nmemb == 0)
+    {
+        nmemb = 2;
     }else
     {
-        size = (parameters_len(m->parameters) + 1) * sizeof(Tensor *);
+        nmemb = nmemb + 1;
     }
-
+    new_parameters = calloc(nmemb, sizeof(Tensor *));
+    if (!new_parameters)
+        return (NULL);
+    if (!m->parameters)
+    {
+        new_parameters[0] = a;
+        new_parameters[1] = NULL;
+    }
+    else
+    {
+        memcpy(new_parameters, m->parameters, (nmemb - 1) * sizeof(Tensor *));
+        new_parameters[nmemb - 2] = a;
+        new_parameters[nmemb - 1]  = NULL;
+    }
+    free(m->parameters);
+    m->parameters = new_parameters;
+    return a;
 }
