0a873fddf053238c21eac3f2a44dddb1404174a3
Karam Koujan <kkoujan@e1r8p2.1337.ma>
2025-11-16 21:21:32 +0100

    working on tensor_relu

--- neural_network/nn_operation.c
+++ neural_network/nn_operation.c
@@ -2,13 +2,31 @@
 #include <math.h>
 
 
-Tensor *relu(Tensor *x)
+Tensor *tensor_relu(Tensor *x)
 {
+    if (x->dtype != FLOAT32 && x->dtype != DOUBLE)
+        return (error_msg("input tensor dtype must be a float or double"), NULL);
+    Tensor *r = tensor_zeros(x->shape, x->num_dims, x->dtype, x->device);
+
     for(int i = 0; i < x->size; i++)
     {
-        double val = ((double *)x->data)[i];
-        if (val <= 0)
-            ((double *)x->data)[i] = 0.0;
+        switch(x->dtype)
+        {
+            case FLOAT32:{
+            float val = ((float *)r->data)[i];
+            if (val <= 0)
+                ((float *)r->data)[i] = 0.0f;
+            break;
+            }
+            case DOUBLE :
+            {
+            double val = ((double *)r->data)[i];
+            if (val <= 0)
+                ((double *)r->data)[i] = 0.0;
+            break;
+            }     
+        }
     }
-    return x;
+    r->is_leaf = 0;
+    return r;
 }
